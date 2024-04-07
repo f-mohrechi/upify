a24bdb8f3d34802b560b64e3f2fc3b09f4521970
mahi <f.mohrechi78@gmail.com>
2024-04-07 22:06:46 +0330

    feat: first section of home, created songBox component

---
+++ src/components/buttons/ViewMoreButton.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function ViewMoreButton() {
+  return (
+    <div>
+      <button className="flex flex-col items-center">
+        <div className="bg-dark-800 rounded-full p-3 flex justify-center items-center w-16 h-16">
+          <img src="/icons/plus.svg" alt="" />
+        </div>
+        <p className="text-center text-white text-lg font-semibold pt-2">
+          View All
+        </p>
+      </button>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import TitleTwoColors from "../components/typography/TitleTwoColors";
+import SongBox from "../components/boxes/SongBox";
+import ViewMoreButton from "../components/buttons/ViewMoreButton";
 
 export default function Home() {
+  const [songs, setSongs] = useState([
+    {
+      id: 1,
+      name: "Whatever It Takes",
+      artist: "Imagne Dragons",
+      img: "/img/song-1.svg",
+      link: "",
+    },
+    {
+      id: 2,
+      name: "Whatever It Takes",
+      artist: "Imagne Dragons",
+      img: "/img/song-1.svg",
+      link: "",
+    },
+    {
+      id: 3,
+      name: "Whatever It Takes",
+      artist: "Imagne Dragons",
+      img: "/img/song-1.svg",
+      link: "",
+    },
+    {
+      id: 4,
+      name: "Whatever It Takes",
+      artist: "Imagne Dragons",
+      img: "/img/song-1.svg",
+      link: "",
+    },
+    {
+      id: 5,
+      name: "Whatever It Takes",
+      artist: "Imagne Dragons",
+      img: "/img/song-1.svg",
+      link: "",
+    },
+  ]);
+
   return (
     <div className="px-20 py-10">
       <div>
         <TitleTwoColors text={"Weekly Top "} colorText={"Songs"} />
+
+        <div className="grid grid-cols-6 gap-x-10 mt-6 items-center">
+          {songs.map((item, index) => {
+            return <SongBox data={item} key={item.id} />;
+          })}
+          <div>
+            <ViewMoreButton />
+          </div>
+        </div>
       </div>
     </div>
   );

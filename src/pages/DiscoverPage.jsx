71fc9c54d75ca6ae0486c4f363786084244fcc7e
mahi <f.mohrechi78@gmail.com>
2024-04-14 12:33:29 +0330

    genre box, first section of discover page

--- src/pages/DiscoverPage.jsx
+++ src/pages/DiscoverPage.jsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import TitleTwoColors from "../components/typography/TitleTwoColors";
+import ViewMoreButton from "../components/buttons/ViewMoreButton";
+import GenreBox from "../components/boxes/GenreBox";
 
 export default function DiscoverPage() {
+  const [playlist, setPlaylist] = useState([
+    {
+      id: 1,
+      name: "Sad Playlist",
+      img: "/img/genre-1.png",
+      link: "/",
+    },
+    {
+      id: 2,
+      name: "Sad Playlist",
+      img: "/img/genre-1.png",
+      link: "/",
+    },
+    {
+      id: 3,
+      name: "Sad Playlist",
+      img: "/img/genre-1.png",
+      link: "/",
+    },
+    {
+      id: 4,
+      name: "Sad Playlist",
+      img: "/img/genre-1.png",
+      link: "/",
+    },
+  ]);
+
   return (
-    <div>
-      <p>DiscoverPage</p>
+    <div className="px-20 py-10">
+      {/* music genres */}
+      <div>
+        <TitleTwoColors text={"Music"} colorText={" Genres"} />
+
+        <div className="grid grid-cols-5 gap-x-10 mt-6 items-center">
+          {playlist.map((item, index) => {
+            return <GenreBox data={item} key={item.id} />;
+          })}
+          <div className="flex justify-end">
+            <ViewMoreButton />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
